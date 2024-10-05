eb73e8c437f69290483a396228b45e1765d2cea3
Sarah Savage <me@sarah-savage.com>
2024-10-05 19:33:37 -0400

    Adding stats utility

---
+++ code/src/Utilities/StatsUtil.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AssetGrabber\Utilities;
+
+use InvalidArgumentException;
+
+abstract class StatsUtil
+{
+    /** @var array<string, int> */
+    private static array $stats = [];
+
+    public const RAW       = true;
+    public const FORMATTED = false;
+
+    public const RESET_ALL = 1;
+
+    public const RESET_VALUES = 2;
+
+    /**
+     * @param array<int, string> $stats
+     */
+    public static function defineStats(array $stats = []): void
+    {
+        foreach ($stats as $stat) {
+            self::defineStat($stat);
+        }
+    }
+
+    public static function defineStat(string $stat): void
+    {
+        if (! isset(self::$stats[$stat])) {
+            self::$stats[$stat] = 0;
+            return;
+        }
+
+        throw new InvalidArgumentException('Unable to add previously defined stat!');
+    }
+
+    public static function add(string $stat, int $amount = 1): void
+    {
+        if (! isset(self::$stats[$stat])) {
+            throw new InvalidArgumentException('Unable to add undefined stat!');
+        }
+
+        self::$stats[$stat] += $amount;
+    }
+
+    public static function increment(string $stat): void
+    {
+        self::add($stat, 1);
+    }
+
+    /**
+     * @return array<string, int>|array<int, string>
+     */
+    public static function getStats(bool $style = self::FORMATTED): array
+    {
+        if ($style === self::RAW) {
+            return self::$stats;
+        }
+
+        $stats   = self::$stats;
+        $longest = 0;
+        $result  = [];
+        foreach ($stats as $stat => $amount) {
+            if (strlen($stat) > $longest) {
+                $longest = strlen($stat) + 1;
+            }
+
+            $result[ucfirst($stat)] = $amount;
+        }
+
+        $formatted = [];
+        foreach ($result as $stat => $amount) {
+            $stat       .= ":";
+            $stat        = str_pad($stat, $longest, ' ', STR_PAD_RIGHT);
+            $formatted[] = $stat . " " . $amount;
+        }
+
+        return $formatted;
+    }
+
+    public static function reset(int $reset = self::RESET_ALL): void
+    {
+        if ($reset === self::RESET_ALL) {
+            self::$stats = [];
+        } else {
+            foreach (self::$stats as $stat => $amount) {
+                self::$stats[$stat] = 0;
+            }
+        }
+    }
+}
---
+++ code/tests/unit/Utilities/StatsUtilTest.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AssetGrabber\Tests\Unit\Utilities;
+
+use AssetGrabber\Utilities\StatsUtil;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class StatsUtilTest extends TestCase
+{
+    public function testStatsDefinedProperly(): void
+    {
+        StatsUtil::defineStats(['foo', 'bar', 'baz']);
+        $stats = StatsUtil::getStats(StatsUtil::RAW);
+        $this->assertEquals(
+            [
+                'foo' => 0,
+                'bar' => 0,
+                'baz' => 0,
+            ],
+            $stats
+        );
+    }
+
+    public function testStatsDefinedOneByOneWorks(): void
+    {
+        StatsUtil::defineStat('foo');
+        StatsUtil::defineStat('bar');
+        StatsUtil::defineStat('baz');
+        $stats = StatsUtil::getStats(StatsUtil::RAW);
+        $this->assertEquals(
+            [
+                'foo' => 0,
+                'bar' => 0,
+                'baz' => 0,
+            ],
+            $stats
+        );
+    }
+
+    public function testCanNotSetStatPreviouslyDefined(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        StatsUtil::defineStat('foo');
+        StatsUtil::defineStat('foo');
+    }
+
+    public function testStatsAddsCorrectly(): void
+    {
+        StatsUtil::defineStats(['foo', 'bar', 'baz']);
+        StatsUtil::add('foo');
+        StatsUtil::add('bar', 1);
+        StatsUtil::add('baz', 2);
+        $stats = StatsUtil::getStats(StatsUtil::RAW);
+        $this->assertEquals(
+            [
+                'foo' => 1,
+                'bar' => 1,
+                'baz' => 2,
+            ],
+            $stats
+        );
+    }
+
+    public function testStatsCanNotAddToNonDefinedStat(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        StatsUtil::defineStats(['foo', 'bar', 'baz']);
+        StatsUtil::add('bin');
+    }
+
+    public function testStatFormattingFormatsCorrectly(): void
+    {
+        StatsUtil::defineStats(['fooshort', 'fooveryverylong', 'baz']);
+        StatsUtil::add('fooshort');
+        StatsUtil::add('fooveryverylong', 2);
+        StatsUtil::add('baz', 3);
+        $stats = StatsUtil::getStats();
+        $this->assertEquals(
+            [
+                'Fooshort:        1',
+                'Fooveryverylong: 2',
+                'Baz:             3',
+            ],
+            $stats
+        );
+    }
+
+    public function testResetStatValuesWorks(): void
+    {
+        StatsUtil::defineStats(['foo', 'bar', 'baz']);
+        StatsUtil::add('foo');
+        StatsUtil::increment('bar');
+        StatsUtil::increment('bar');
+        StatsUtil::add('baz', 4);
+        $stats = StatsUtil::getStats(StatsUtil::RAW);
+        $this->assertEquals(
+            [
+                'foo' => 1,
+                'bar' => 2,
+                'baz' => 4,
+            ],
+            $stats
+        );
+
+        StatsUtil::reset(StatsUtil::RESET_VALUES);
+        $stats = StatsUtil::getStats(StatsUtil::RAW);
+        $this->assertEquals(
+            [
+                'foo' => 0,
+                'bar' => 0,
+                'baz' => 0,
+            ],
+            $stats
+        );
+    }
+
+    protected function tearDown(): void
+    {
+        StatsUtil::reset();
+    }
+}
