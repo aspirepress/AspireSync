29abb2e19fb05b79b6c1f7b9797940bb58b64da4
Chuck Adams <cja987@gmail.com>
2024-10-30 21:36:01 -0600

    fix: use 1.2 api with all fields for plugin metadata

--- src/Services/WPEndpointClient.php
+++ src/Services/WPEndpointClient.php
@@ -5,27 +5,65 @@ declare(strict_types=1);
 namespace AspirePress\AspireSync\Services;
 
 use AspirePress\AspireSync\Services\Interfaces\WpEndpointClientInterface;
+use AspirePress\AspireSync\Utilities\FileUtil;
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Exception\ClientException;
+use function Safe\json_decode;
 
 class WPEndpointClient implements WpEndpointClientInterface
 {
-    public function __construct(private readonly GuzzleClient $guzzle)
-    {
-    }
+    public function __construct(private readonly GuzzleClient $guzzle) {}
 
     public function getPluginMetadata(string $plugin): string
     {
-        $url = 'https://api.wordpress.org/plugins/info/1.0/' . $plugin . '.json';
+        $url = 'https://api.wordpress.org/plugins/info/1.2/';
+        $queryParams = [
+            'action' => 'plugin_information',
+            'slug' => $plugin,
+            'fields' => [
+                'active_installs',
+                'added',
+                'author',
+                'author_block_count',
+                'author_block_rating',
+                'author_profile',
+                'banners',
+                'compatibility',
+                'contributors',
+                'description',
+                'donate_link',
+                'download_link',
+                'downloaded',
+                'homepage',
+                'icons',
+                'last_updated',
+                'name',
+                'num_ratings',
+                'rating',
+                'ratings',
+                'requires',
+                'requires_php',
+                'screenshots',
+                'sections',
+                'short_description',
+                'slug',
+                'support_threads',
+                'support_threads_resolved',
+                'tags',
+                'tested',
+                'version',
+                'versions',
+            ],
+        ];
+
         try {
-            $response = $this->guzzle->get($url);
+            $response = $this->guzzle->get($url, ['query' => $queryParams]);
             return $response->getBody()->getContents();
         } catch (ClientException $e) {
             if ($e->getCode() === 404) {
                 return $e->getResponse()->getBody()->getContents();
             }
         }
-
         return '';
     }
 
