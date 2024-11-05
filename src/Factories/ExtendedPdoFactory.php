39dfbac4a53a371f7fe338ddfbddd4a7f2a856ed
Chuck Adams <cja987@gmail.com>
2024-11-04 17:39:29 -0700

    fix: enable foreign key pragma

--- src/Factories/ExtendedPdoFactory.php
+++ src/Factories/ExtendedPdoFactory.php
@@ -6,22 +6,25 @@ namespace AspirePress\AspireSync\Factories;
 
 use Aura\Sql\ExtendedPdo;
 use Laminas\ServiceManager\ServiceManager;
+use PDOException;
 
 class ExtendedPdoFactory
 {
     public function __invoke(ServiceManager $serviceManager): ExtendedPdo
     {
         $config = $serviceManager->get('config');
-        $pdo = new ExtendedPdo($config['database']['dsn']);
+        $pdo    = new ExtendedPdo($config['database']['dsn']);
 
         return $this->initialize($pdo, $serviceManager);
     }
 
-    private function initialize(ExtendedPdo $pdo, ServiceManager $serviceManager): ExtendedPdo {
+    private function initialize(ExtendedPdo $pdo, ServiceManager $serviceManager): ExtendedPdo
+    {
         $pdo->connect();
+        $pdo->exec('PRAGMA foreign_keys = ON');
         try {
             $pdo->query("select 1 from sync_plugins limit 1");
-        } catch (\PDOException $e) {
+        } catch (PDOException $e) {
             $init_script = $serviceManager->get('config')['database']['init_script'];
             $pdo->exec(file_get_contents($init_script));
         }
