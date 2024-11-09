413df6824fdf45c08c125ad529b92c2f3f937b38
Chuck Adams <cja987@gmail.com>
2024-11-09 00:53:12 -0700

    chore: rename export -> dump

--- src/Commands/Plugins/PluginsMetaDumpCommand.php
+++ src/Commands/Plugins/PluginsMetaDumpCommand.php
@@ -5,17 +5,12 @@ declare(strict_types=1);
 namespace AspirePress\AspireSync\Commands\Plugins;
 
 use AspirePress\AspireSync\Commands\AbstractBaseCommand;
-use AspirePress\AspireSync\Services\Interfaces\WpEndpointClientInterface;
-use AspirePress\AspireSync\Services\Plugins\PluginListService;
 use AspirePress\AspireSync\Services\Plugins\PluginMetadataService;
-use AspirePress\AspireSync\Services\StatsMetadataService;
-use AspirePress\AspireSync\Utilities\StringUtil;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class PluginsMetaExportCommand extends AbstractBaseCommand
+class PluginsMetaDumpCommand extends AbstractBaseCommand
 {
     public function __construct(
         private PluginMetadataService $meta,
@@ -25,7 +20,7 @@ class PluginsMetaExportCommand extends AbstractBaseCommand
 
     protected function configure(): void
     {
-        $this->setName('plugins:meta:export')
+        $this->setName('plugins:meta:dump')
             ->setDescription('Dumps metadata of all plugins in jsonl format');
     }
 
--- src/Commands/Themes/ThemesMetaDumpCommand.php
+++ src/Commands/Themes/ThemesMetaDumpCommand.php
@@ -5,17 +5,12 @@ declare(strict_types=1);
 namespace AspirePress\AspireSync\Commands\Themes;
 
 use AspirePress\AspireSync\Commands\AbstractBaseCommand;
-use AspirePress\AspireSync\Services\Interfaces\WpEndpointClientInterface;
-use AspirePress\AspireSync\Services\Themes\ThemeListService;
 use AspirePress\AspireSync\Services\Themes\ThemeMetadataService;
-use AspirePress\AspireSync\Services\StatsMetadataService;
-use AspirePress\AspireSync\Utilities\StringUtil;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ThemesMetaExportCommand extends AbstractBaseCommand
+class ThemesMetaDumpCommand extends AbstractBaseCommand
 {
     public function __construct(
         private ThemeMetadataService $meta,
@@ -25,7 +20,7 @@ class ThemesMetaExportCommand extends AbstractBaseCommand
 
     protected function configure(): void
     {
-        $this->setName('themes:meta:export')
+        $this->setName('themes:meta:dump')
             ->setDescription('Dumps metadata of all themes in jsonl format');
     }
 
