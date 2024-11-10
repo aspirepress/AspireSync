ac5770b2de5b55d377d3830f3a302c4dbf56f127
Chuck Adams <cja987@gmail.com>
2024-11-10 14:15:35 -0700

    refactor: use native workflow rather than docker

--- docker/init-db.sh
+++
@@ -1,4 +0,0 @@
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER aspiresync PASSWORD 'password';
-	CREATE DATABASE aspiresync WITH OWNER = 'aspiresync';
-EOSQL
