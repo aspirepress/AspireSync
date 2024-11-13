874d248d6adba71ea54c237bcd349ae33cc559ca
Chuck Adams <cja987@gmail.com>
2024-11-13 08:17:57 -0700

    refactor: fix IDE warnings in Logger.php, strip out doc comments

--- src/Services/Logger.php
+++ src/Services/Logger.php
@@ -14,35 +14,12 @@ use DateTime;
 use Psr\Log\AbstractLogger;
 use Psr\Log\LogLevel;
 use RuntimeException;
-
-/**
- * Finally, a light, permissions-checking logging class.
- *
- * Originally written for use with wpSearch
- *
- * Usage:
- * $log = new Katzgrau\KLogService\LogService('/var/log/', Psr\Log\LogLevel::INFO);
- * $log->info('Returned a million search results'); //Prints to the log file
- * $log->error('Oh dear.'); //Prints to the log file
- * $log->debug('x = 5'); //Prints nothing due to current severity threshhold
- *
- * @link    https://github.com/katzgrau/KLogger
- *
- * @since   July 26, 2008
- */
+use Stringable;
 
 class Logger extends AbstractLogger
 {
-    /**
-     * KLogger options
-     *  Anything options not considered 'core' to the logging library should be
-     *  settable view the third parameter in the constructor
-     *
-     *  Core options include the log file path and the log threshold
-     *
-     * @var array
-     */
-    protected $options = [
+    // TODO: nuke and/or hardwire almost all of this
+    protected array $options = [
         'extension'      => 'txt',
         'dateFormat'     => 'Y-m-d G:i:s.u',
         'filename'       => false,
@@ -52,33 +29,14 @@ class Logger extends AbstractLogger
         'appendContext'  => true,
     ];
 
-    /**
-     * Path to the log file
-     *
-     * @var string
-     */
-    private $logFilePath;
+    private string $logFilePath;
 
-    /**
-     * Current minimum logging threshold
-     *
-     * @var integer
-     */
-    protected $logLevelThreshold = LogLevel::DEBUG;
+    protected string|int $logLevelThreshold = LogLevel::DEBUG;  // TODO make this just int and translate PSR-3 levels to it
 
-    /**
-     * The number of lines logged in this instance's lifetime
-     *
-     * @var int
-     */
-    private $logLineCount = 0;
+    private int $logLineCount = 0;  // TODO: nuke
 
-    /**
-     * Log Levels
-     *
-     * @var array
-     */
-    protected $logLevels = [
+    // could be a const, or maybe just made public so one can tweak them in edge cases
+    protected array $logLevels = [
         LogLevel::EMERGENCY => 0,
         LogLevel::ALERT     => 1,
         LogLevel::CRITICAL  => 2,
@@ -89,37 +47,14 @@ class Logger extends AbstractLogger
         LogLevel::DEBUG     => 7,
     ];
 
-    /**
-     * This holds the file handle for this instance's log file
-     *
-     * @var resource
-     */
+    /** @var resource */
     private $fileHandle;
 
-    /**
-     * This holds the last line logged to the logger
-     *  Used for unit tests
-     *
-     * @var string
-     */
-    private $lastLine = '';
+    private string $lastLine = '';  // TODO: nuke (used only in unit tests)
 
-    /**
-     * Octal notation for default permissions of the log file
-     *
-     * @var integer
-     */
-    private $defaultPermissions = 0777;
+    private int $defaultPermissions = 0777; // modified by umask one hopes.  XXX still no reason to make it +x tho.
 
-    /**
-     * @internal param string $logFilePrefix The prefix for the log file name
-     * @internal param string $logFileExt The extension for the log file
-     *
-     * @param string $logDirectory File path to the logging directory
-     * @param string $logLevelThreshold The LogLevel Threshold
-     * @param array $options
-     */
-    public function __construct($logDirectory, $logLevelThreshold = LogLevel::DEBUG, array $options = [])
+    public function __construct(string $logDirectory, string $logLevelThreshold = LogLevel::DEBUG, array $options = [])
     {
         $this->logLevelThreshold = $logLevelThreshold;
         $this->options           = array_merge($this->options, $options);
@@ -129,7 +64,7 @@ class Logger extends AbstractLogger
             mkdir($logDirectory, $this->defaultPermissions, true);
         }
 
-        if (strpos($logDirectory, 'php://') === 0) {
+        if (str_starts_with($logDirectory, 'php://')) {
             $this->setLogToStdOut($logDirectory);
             $this->setFileHandle('w+');
         } else {
@@ -145,24 +80,17 @@ class Logger extends AbstractLogger
         }
     }
 
-    /**
-     * @param string $stdOutPath
-     */
-    public function setLogToStdOut($stdOutPath)
+    public function setLogToStdOut(string $stdOutPath): void
     {
         $this->logFilePath = $stdOutPath;
     }
 
-    /**
-     * @param string $logDirectory
-     */
-    public function setLogFilePath($logDirectory)
+    public function setLogFilePath(string $logDirectory): void
     {
         if ($this->options['filename']) {
             if (
-                strpos($this->options['filename'], '.log') !== false
-                || strpos($this->options['filename'], '.txt')
-                !== false
+                str_contains($this->options['filename'], '.log')
+                || str_contains($this->options['filename'], '.txt')
             ) {
                 $this->logFilePath = $logDirectory . DIRECTORY_SEPARATOR . $this->options['filename'];
             } else {
@@ -182,12 +110,7 @@ class Logger extends AbstractLogger
         }
     }
 
-    /**
-     * @internal param resource $fileHandle
-     *
-     * @param $writeMode
-     */
-    public function setFileHandle($writeMode)
+    public function setFileHandle(string $writeMode): void
     {
         $this->fileHandle = fopen($this->logFilePath, $writeMode);
     }
@@ -199,35 +122,17 @@ class Logger extends AbstractLogger
         }
     }
 
-    /**
-     * Sets the date format used by all instances of KLogger
-     *
-     * @param string $dateFormat Valid format string for date()
-     */
-    public function setDateFormat($dateFormat)
+    public function setDateFormat(string $dateFormat): void
     {
         $this->options['dateFormat'] = $dateFormat;
     }
 
-    /**
-     * Sets the Log Level Threshold
-     *
-     * @param string $logLevelThreshold The log level threshold
-     */
-    public function setLogLevelThreshold($logLevelThreshold)
+    public function setLogLevelThreshold(string $logLevelThreshold): void
     {
         $this->logLevelThreshold = $logLevelThreshold;
     }
 
-    /**
-     * Logs with an arbitrary level.
-     *
-     * @param mixed $level
-     * @param string $message
-     * @param array $context
-     * @return null
-     */
-    public function log($level, $message, array $context = []): void
+    public function log(mixed $level, string|Stringable $message, array $context = []): void
     {
         if ($this->logLevels[$this->logLevelThreshold] < $this->logLevels[$level]) {
             return;
@@ -236,13 +141,7 @@ class Logger extends AbstractLogger
         $this->write($message);
     }
 
-    /**
-     * Writes a line to the log without prepending a status or timestamp
-     *
-     * @param string $message Line to write to the log
-     * @return void
-     */
-    public function write($message)
+    public function write(string|Stringable $message): void
     {
         if (null !== $this->fileHandle) {
             if (fwrite($this->fileHandle, $message) === false) {
@@ -258,35 +157,17 @@ class Logger extends AbstractLogger
         }
     }
 
-    /**
-     * Get the file path that the log is currently writing to
-     *
-     * @return string
-     */
-    public function getLogFilePath()
+    public function getLogFilePath(): string
     {
         return $this->logFilePath;
     }
 
-    /**
-     * Get the last line logged to the log file
-     *
-     * @return string
-     */
-    public function getLastLogLine()
+    public function getLastLogLine(): string
     {
         return $this->lastLine;
     }
 
-    /**
-     * Formats the message for logging.
-     *
-     * @param string $level The Log Level of the message
-     * @param string $message The message to log
-     * @param array $context The context
-     * @return string
-     */
-    protected function formatMessage($level, $message, $context)
+    protected function formatMessage(string $level, string $message, array $context): string
     {
         if ($this->options['logFormat']) {
             $parts   = [
@@ -295,7 +176,7 @@ class Logger extends AbstractLogger
                 'level-padding' => str_repeat(' ', 9 - strlen($level)),
                 'priority'      => $this->logLevels[$level],
                 'message'       => $message,
-                'context'       => json_encode($context),
+                'context'       => json_encode($context, JSON_THROW_ON_ERROR),
             ];
             $message = $this->options['logFormat'];
             foreach ($parts as $part => $value) {
@@ -312,15 +193,7 @@ class Logger extends AbstractLogger
         return $message . PHP_EOL;
     }
 
-    /**
-     * Gets the correctly formatted Date/Time for the log entry.
-     *
-     * PHP DateTime is dump, and you have to resort to trickery to get microseconds
-     * to work correctly, so here it is.
-     *
-     * @return string
-     */
-    private function getTimestamp()
+    private function getTimestamp(): string
     {
         $originalTime = microtime(true);
         $micro        = sprintf("%06d", ($originalTime - floor($originalTime)) * 1000000);
@@ -329,13 +202,8 @@ class Logger extends AbstractLogger
         return $date->format($this->options['dateFormat']);
     }
 
-    /**
-     * Takes the given context and coverts it to a string.
-     *
-     * @param array $context The Context
-     * @return string
-     */
-    protected function contextToString($context)
+    // TODO: nuke this with extreme prejudice
+    protected function contextToString(array $context): string
     {
         $export = '';
         foreach ($context as $key => $value) {
@@ -343,7 +211,7 @@ class Logger extends AbstractLogger
             $export .= preg_replace([
                 '/=>\s+([a-zA-Z])/im',
                 '/array\(\s+\)/im',
-                '/^  |\G  /m',
+                '/^ {2}|\G {2}/m',
             ], [
                 '=> $1',
                 'array()',
@@ -354,14 +222,8 @@ class Logger extends AbstractLogger
         return str_replace(['\\\\', '\\\''], ['\\', '\''], rtrim($export));
     }
 
-    /**
-     * Indents the given string with the given indent.
-     *
-     * @param string $string The string to indent
-     * @param string $indent What to use as the indent.
-     * @return string
-     */
-    protected function indent($string, $indent = '    ')
+    // TODO: nuke (json isn't going to need indenting)
+    protected function indent($string, $indent = '    '): string
     {
         return $indent . str_replace("\n", "\n" . $indent, $string);
     }
