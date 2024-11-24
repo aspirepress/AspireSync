7fe5cae7da07a81310cf00340ea1ba72976d2fce
Chuck Adams <cja987@gmail.com>
2024-11-24 16:29:58 -0700

    feat: RegexUtil::replace and _replace methods

--- src/Utilities/RegexUtil.php
+++ src/Utilities/RegexUtil.php
@@ -1,16 +1,26 @@
 <?php
+
 declare(strict_types=1);
 
 namespace AspirePress\AspireSync\Utilities;
 
 // This class is normally just named 'Regex' elsewhere, but it gets the "Util" suffix in AS for consistency.
 
-class RegexUtil {
+use Safe\Exceptions\PcreException;
+
+use function Safe\preg_grep;
+use function Safe\preg_match;
+use function Safe\preg_match_all;
+use function Safe\preg_replace;
+use function Safe\preg_split;
+
+class RegexUtil
+{
     /** @return string[] */
     public static function match(string $pattern, string $subject, int $flags = 0, int $offset = 0): array
     {
         $matches = [];
-        \Safe\preg_match($pattern, $subject, $matches, $flags, $offset);
+        preg_match($pattern, $subject, $matches, $flags, $offset);
         return $matches;
     }
 
@@ -18,7 +28,7 @@ class RegexUtil {
     public static function matchAll(string $pattern, string $subject, int $flags = 0, int $offset = 0): array
     {
         $matches = [];
-        \Safe\preg_match_all($pattern, $subject, $matches, $flags, $offset);
+        preg_match_all($pattern, $subject, $matches, $flags, $offset);
         return $matches;
     }
 
@@ -29,7 +39,7 @@ class RegexUtil {
      */
     public static function split(string $pattern, string $subject, ?int $limit = -1, int $flags = 0): array
     {
-        return \Safe\preg_split($pattern, $subject, $limit, $flags);
+        return preg_split($pattern, $subject, $limit, $flags);
     }
 
     /**
@@ -39,7 +49,34 @@ class RegexUtil {
      */
     public static function grep(string $pattern, array $array, int $flags = 0): array
     {
-        return \Safe\preg_grep($pattern, $array, $flags);
+        return preg_grep($pattern, $array, $flags);
+    }
+
+    /**
+     * Fits the most common use case of preg_replace, namely single strings.  Does not support the by-ref $count arg.
+     */
+    public static function replace(string $pattern, string $replacement, string $subject, int $limit = -1): string
+    {
+        return static::_replace($pattern, $replacement, $subject, $limit);
+    }
+
+    /**
+     * Equivalent to \Safe\preg_replace, inherits the insane signature of the builtin preg_replace
+     *
+     * @param string|string[] $pattern
+     * @param string|string[] $replacement
+     * @param string|string[] $subject
+     * @return string|string[]
+     * @throws PcreException
+     */
+    public static function _replace(
+        string|array $pattern,
+        string|array $replacement,
+        string|array $subject,
+        int $limit = -1,
+        ?int &$count = null
+    ): string|array {
+        return preg_replace($pattern, $replacement, $subject, $limit, $count);
     }
 
     private function __construct()
