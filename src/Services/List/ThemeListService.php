f5ad5215999b4033c18863ed9ce818a40ea17fd6
Chuck Adams <cja987@gmail.com>
2024-11-28 18:14:06 -0700

    refactor: inject SubversionServiceInterface rather than concrete type

--- src/Services/List/ThemeListService.php
+++ src/Services/List/ThemeListService.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace AspirePress\AspireSync\Services\List;
 
-use AspirePress\AspireSync\Services\List\AbstractListService;
+use AspirePress\AspireSync\Services\Interfaces\SubversionServiceInterface;
 use AspirePress\AspireSync\Services\Metadata\ThemeMetadataService;
 use AspirePress\AspireSync\Services\RevisionMetadataService;
-use AspirePress\AspireSync\Services\SubversionService;
 
 readonly class ThemeListService extends AbstractListService
 {
-    public function __construct(SubversionService $svn, ThemeMetadataService $meta, RevisionMetadataService $revisions)
+    public function __construct(SubversionServiceInterface $svn, ThemeMetadataService $meta, RevisionMetadataService $revisions)
     {
         parent::__construct($svn, $meta, $revisions, 'themes');
     }
