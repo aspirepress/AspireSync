b0f42a6d14df40537d14eddcaff1f4b684e84bce
Chuck Adams <cja987@gmail.com>
2025-01-25 16:37:38 -0700

    tweak: split slugs on commas or whitespace instead of just commas

--- src/Utilities/StringUtil.php
+++ src/Utilities/StringUtil.php
@@ -7,9 +7,9 @@ namespace App\Utilities;
 abstract class StringUtil
 {
     /** @return string[] */
-    public static function explodeAndTrim(string $string, string $separator = ','): array
+    public static function explodeAndTrim(string $string, string $pattern = '/[\s,;]+/'): array
     {
-        $parts = explode($separator, $string);
+        $parts = \Safe\preg_split($pattern, $string);
         array_walk($parts, fn(&$str) => $str = trim($str));
         return array_values(array_filter($parts, fn($str) => $str !== ''));
     }
