ab9cca710d8b30a9399341b8c5fa5b86ed3f92f1
Chuck Adams <cja987@gmail.com>
2025-02-13 14:40:34 -0700

    fix: rm unused methods from MetadataServiceStub

--- tests/Stubs/MetadataServiceStub.php
+++ tests/Stubs/MetadataServiceStub.php
@@ -9,16 +9,6 @@ use Generator;
 
 class MetadataServiceStub implements MetadataServiceInterface
 {
-    public function getUnprocessedVersions(string $slug, array $versions): array
-    {
-        return [];
-    }
-
-    public function getDownloadUrl(string $slug, string $version): ?string
-    {
-        return "https://example.org/download/$slug.$version";
-    }
-
     public function exportAllMetadata(): Generator
     {
         yield from [];
@@ -32,16 +22,6 @@ class MetadataServiceStub implements MetadataServiceInterface
     public function getStatus(string $slug): ?string
     {
         return 'open';
-    }
-
-    public function getOpenVersions(?int $timestamp = 1): array
-    {
-        return [];
-    }
-
-    public function markProcessed(string $slug, string $version): void
-    {
-        // nothingness
     }
 
     public function getPulledAfter(int $timestamp): array
