80eeb6b727593ee35b35cdf12d874c741b04cd54
Chuck Adams <cja987@gmail.com>
2025-02-22 16:19:59 -0700

    Trim unused packages (#50)
    * refactor: zap doctrine/orm (but still using DBAL)
    
    * refactor: zap flysystem
    
    * build: upgrade dev deps
    
    * refactor: zap CacheServiceInterface and DatabaseCacheService
    
    * refactor: zap VersionUtil

--- src/Services/Metadata/PluginMetadataService.php
+++ src/Services/Metadata/PluginMetadataService.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace App\Services\Metadata;
 
 use App\ResourceType;
-use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\DBAL\Connection;
 use Psr\Log\LoggerInterface;
 
 readonly class PluginMetadataService extends AbstractMetadataService
 {
-    public function __construct(EntityManagerInterface $em, LoggerInterface $log)
+    public function __construct(Connection $conn, LoggerInterface $log)
     {
-        parent::__construct($em, $log, ResourceType::Plugin);
+        parent::__construct($conn, $log, ResourceType::Plugin);
     }
 }
