6a63798e17ce7a6132265ddbadfe2c0eda891ced
Chuck Adams <chaz@chaz.works>
2025-09-26 10:05:43 -0600

    run pint for probably the last time

--- src/Integrations/GitUpdater/UpdateApiRequest.php
+++ src/Integrations/GitUpdater/UpdateApiRequest.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace App\Integrations\GitUpdater;
 
-
-use Override;
-
 class UpdateApiRequest extends AbstractGitUpdaterRequest
 {
     public function __construct(public readonly string $slug) {}
